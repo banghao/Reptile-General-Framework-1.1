3ceac403fb961140fc0956ea1df8a384e1d3d8f1
banghao <969306802@qq.com>
2018-04-28 22:41:16 +0800

    Chinese University ranking reptiles

---
+++ pythonRankFrame/大学排名爬虫框架.py
@@ -0,0 +1,29 @@
+/*中国大学定向排名*/
+import requests
+from bs4 import BeautifulSoup import bs4
+def getHTMLTexst(url):
+    try:
+        r=requests.get(url,timeout=30)
+        r.raise_for_status()
+        r.encoding=r.apparent_encoding
+        return r.text
+    except:
+        return ""
+def fillUnivList(ulist,html):
+    soup=BeautifulSoup(html,"html.parser")
+    for tr in soup.find('today').children:
+        if isinsstance(tr,bs4.element.tag):
+            tds=tr('td')
+            ulist.append([tds[0].string, tds[1].string, tds[3].string])
+def printUnivList(ulist, num):
+    plt = "{0:^10}\t{1:{3}^10}\t{2:^10}"
+    print(tplt.format("排名","学校名称","总分",chr(12288)))
+    for i in range(num):
+      u=ulist[i]
+         print(tplt.format(u[0],u[1],u[2],chr(12288)))
+ def main():  
+     uinfo = []
+   url = 'https://www.zuihaodaxue.cn/zuihaodaxuepaiming2016.html'
+     html = getHTMLText(url)   
+     fillUnivList(uinfo, html)  
+     printUnivList(uinfo, 20) # 20 univs main()
